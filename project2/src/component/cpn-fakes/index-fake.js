86ef268e6e03d05a48a3b68ce348f34069b46edb
thiennhat <thiennhat3004@gmail.com>
2019-05-13 08:58:10 +0700

    thay đổi nè

---
+++ project2/src/component/cpn-fakes/index-fake.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class IndexFake extends Component {
+    render() {
+        return (
+            <div>
+                thay đổi nè
+            </div>
+        );
+    }
+}
+
+export default IndexFake;
