cf27da15834f6efcbcc2f126e48681ab71319e31
thiennhat <thiennhat3004@gmail.com>
2019-05-21 09:51:36 +0700

    better1

---
+++ project2/build/precache-manifest.0f14c2bbb0d017c801850801f73a5fd5.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d099b7c9026872b67518d9b8b72310a",
+    "url": "/static/media/new10.6d099b7c.png"
+  },
+  {
+    "revision": "befa1fdd5934dba950dc",
+    "url": "/static/css/main.5d450881.chunk.css"
+  },
+  {
+    "revision": "cd3606f15a305b6bc516",
+    "url": "/static/js/1.cd3606f1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6de023df87bbeb77ebb6987f35b4aabd",
+    "url": "/static/media/news-1.6de023df.jpg"
+  },
+  {
+    "revision": "a921ed387bed9d84e9f1dccce485c58b",
+    "url": "/static/media/slide.a921ed38.png"
+  },
+  {
+    "revision": "7834cfed5666518140818f1ad2570373",
+    "url": "/static/media/new11.7834cfed.png"
+  },
+  {
+    "revision": "586e46cf37dca09734e319c686c69935",
+    "url": "/static/media/gs-shop.586e46cf.png"
+  },
+  {
+    "revision": "0a9f86236ecc5138116690da6163e861",
+    "url": "/static/media/news-2.0a9f8623.jpg"
+  },
+  {
+    "revision": "e685edf62a98cc948380c2ce9d483a2a",
+    "url": "/static/media/danhdu.e685edf6.png"
+  },
+  {
+    "revision": "b43b9246fed360e160e81ddb54cda654",
+    "url": "/static/media/dalam.b43b9246.png"
+  },
+  {
+    "revision": "16b6ce5a2fa049ddd7359a478be6903a",
+    "url": "/static/media/caychanchim.16b6ce5a.png"
+  },
+  {
+    "revision": "c0bf73d8f452837d4630e827818a6f11",
+    "url": "/static/media/cota.c0bf73d8.png"
+  },
+  {
+    "revision": "1e766106aa7d0ab19b93b2b07ae3aab2",
+    "url": "/static/media/new3.1e766106.png"
+  },
+  {
+    "revision": "ca9047dd2221ef03776e70793a3a9a08",
+    "url": "/static/media/new2.ca9047dd.png"
+  },
+  {
+    "revision": "347da1aa0efbff27442946516018f261",
+    "url": "/static/media/new4.347da1aa.png"
+  },
+  {
+    "revision": "3d400de8d7f88ce7b4d0c3f0d9f323ee",
+    "url": "/static/media/hoa-bg.3d400de8.png"
+  },
+  {
+    "revision": "bf53f1dcc32b7f90da909fa02b6b4e43",
+    "url": "/static/media/bg-header.bf53f1dc.png"
+  },
+  {
+    "revision": "48d2724e4c76eda45fa2916a108f1f4a",
+    "url": "/static/media/bg-home.48d2724e.png"
+  },
+  {
+    "revision": "8b99cc80647927e00270b04c73289d7d",
+    "url": "/static/media/bg-section.8b99cc80.png"
+  },
+  {
+    "revision": "d33150ac17793a68df6d36e8ffadb681",
+    "url": "/static/media/new1.d33150ac.png"
+  },
+  {
+    "revision": "befa1fdd5934dba950dc",
+    "url": "/static/js/main.befa1fdd.chunk.js"
+  },
+  {
+    "revision": "300ac042baaf17acc312400dbf73df76",
+    "url": "/static/media/new12.300ac042.png"
+  },
+  {
+    "revision": "b62bea289ce67fb15fc5830115e20a67",
+    "url": "/static/media/new13.b62bea28.png"
+  },
+  {
+    "revision": "fc237b69c093a11ce3c406ba7fdbeb28",
+    "url": "/static/media/spx2-14.fc237b69.png"
+  },
+  {
+    "revision": "b57f5f84ccb99c1beeef7ceec9b1d818",
+    "url": "/static/media/menu-info.b57f5f84.jpg"
+  },
+  {
+    "revision": "861ec30c6951fb2c5dfe4bf094bbf9a7",
+    "url": "/static/media/footer-header.861ec30c.png"
+  },
+  {
+    "revision": "65ba4c732a51ce023d4cfca2fec0365c",
+    "url": "/static/media/about-adv-img.65ba4c73.jpg"
+  },
+  {
+    "revision": "c1a2e3195c9b251c7c8cb30205eed2c1",
+    "url": "/static/media/UTM Caviar.c1a2e319.ttf"
+  },
+  {
+    "revision": "75cde7a913630473b1207d3727eadda9",
+    "url": "/static/media/Lobster_1_4.75cde7a9.otf"
+  },
+  {
+    "revision": "7251457621b9c0e91a909d8f285586ba",
+    "url": "/static/media/UTM Alpine KT.72514576.ttf"
+  },
+  {
+    "revision": "88a04409a9d4017cb17ed93d67ffe2ad",
+    "url": "/static/media/UTM Aquarelle.88a04409.ttf"
+  },
+  {
+    "revision": "446ada2ae8366657a0e32de10940898e",
+    "url": "/static/media/UVF Blenda Script.446ada2a.ttf"
+  },
+  {
+    "revision": "14147ca3288fa08b850d029193150722",
+    "url": "/static/media/Roboto-Thin.14147ca3.ttf"
+  },
+  {
+    "revision": "e2da884523548d2c0592107689cdc106",
+    "url": "/static/media/UVF Didot.e2da8845.ttf"
+  },
+  {
+    "revision": "911cb37d7b59ddaa34dc0c1c4ea5e397",
+    "url": "/static/media/UVF Aphrodite Pro.911cb37d.ttf"
+  },
+  {
+    "revision": "3bcf51dd04830767395aaeefb96d1408",
+    "url": "/static/media/detail-blog-1.3bcf51dd.jpg"
+  },
+  {
+    "revision": "3aad3c88eac1b3f3af72970c6dbaefd5",
+    "url": "/static/media/detail-blog-4.3aad3c88.jpg"
+  },
+  {
+    "revision": "49f78222d41dc2c5fd93934eabeb12c7",
+    "url": "/static/media/detail-blog-5.49f78222.jpg"
+  },
+  {
+    "revision": "853fd3573ab025ad2ca7ea6340d2dc34",
+    "url": "/static/media/title.853fd357.png"
+  },
+  {
+    "revision": "2145c04867d36c7fedad5cbaec72b890",
+    "url": "/static/media/avatar.2145c048.jpg"
+  },
+  {
+    "revision": "e95ef6eab94f20a3bbcf64d3dfe90911",
+    "url": "/index.html"
+  }
+];
